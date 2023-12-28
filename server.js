28c9b9ae77b0fcad8c0b036d9de53468983f316d
Axyannnn1203 <145359342+Axyannnn1203@users.noreply.github.com>
2023-12-28 12:42:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3003;
+
+
+mongoose.connect('mongodb://localhost:27017/registration-form', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: { type: String, unique: true },
+    password: String,
+    gender: String,
+    acceptTerms: Boolean,
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+// Middleware for parsing form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Route for serving the registration form
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Route for handling user registration
+app.post('/register', (req, res) => {
+    const { firstName, lastName, email, password, gender, acceptTerms } = req.body;
+
+    // Create a new user instance
+    const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+        gender,
+        acceptTerms: acceptTerms === 'on', // Convert checkbox value to a boolean
+    });
+
+    // Save the user to the database
+    newUser.save((err) => {
+        if (err) {
+            console.error(err);
+            res.send('Error registering user.');
+        } else {
+            res.send('User registered successfully!');
+        }
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
